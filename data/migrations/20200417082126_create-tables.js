48d4f8547e974d70163bb845c2cc7b2ba6402876
fbzr <fabricio.spb@gmail.com>
2020-04-17 08:42:40 -0700

    create and run create-tables migration, create api routes

--- data/migrations/20200417082126_create-tables.js
+++ data/migrations/20200417082126_create-tables.js
@@ -1,8 +1,60 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('projects', tbl => {
+        tbl.increments();
+        tbl.string('name', 128).notNullable();
+        tbl.string('description');
+        tbl.boolean('completed')
+            .notNullable()
+            .defaultTo(false);
+    })
+    .createTable('resources', tbl => {
+        tbl.increments();
+        tbl.string('name', 128)
+            .unique()
+            .notNullable();
+        tbl.string('description');
+    })
+    .createTable('projects_resources', tbl => {
+        tbl.increments();
+        tbl.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('projects')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+        tbl.integer('resource_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('resources')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+        tbl.unique(['project_id', 'resource_id']);
+    })
+    .createTable('tasks', tbl => {
+        tbl.increments();
+        tbl.string('description')
+            .notNullable();
+        tbl.string('notes');
+        tbl.boolean('completed')
+            .notNullable()
+            .defaultTo(false);
+        tbl.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('projects')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+        tbl.unique(['id', 'project_id']);
+    });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('tasks')
+        .dropTableIfExists('projects_resources')
+        .dropTableIfExists('resources')
+        .dropTableIfExists('projects');
 };
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
 const server = express();
+server.use(express.json());
+
+// routes
+server.use('/api/resources', require('./api/routes/resources'));
+server.use('/api/projects', require('./api/routes/projects'));
 
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => console.log(`**** Server running on port ${PORT} ****`));
